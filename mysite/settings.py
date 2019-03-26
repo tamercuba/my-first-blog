963dab8cdab59b1d6080bcd907f3246f52e90c20
tamercuba <tamercuba@gmail.com>
2019-03-26 16:21:44 -0300

    Changes commited

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=5wqg1bf-^)8_nfpvlkrm1+fa8z7rw=_30ws(n26z+-6yh=8=t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
